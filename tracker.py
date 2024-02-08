b780588dcf5e300e860444a670c3b3fb6b7c6693
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-02-08 10:19:29 +0000

    Created new file, tracker.py and added main function

---
+++ tracker.py
@@ -0,0 +1,12 @@
+def main():
+    print(f"🥳 Running Expense App")
+    # Get input for expense
+    
+    # Write expense to file 
+    
+    # Read file and summarize 
+    pass
+
+
+if __name__ == "__main__":
+    main()
