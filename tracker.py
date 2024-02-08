bd02804c24728ba71fe1f6fc042066db171c321c
Nathan <127421398+nathan-cool@users.noreply.github.com>
2024-02-08 10:26:05 +0000

    Added functions I'll need to use

--- tracker.py
+++ tracker.py
@@ -1,12 +1,28 @@
 def main():
     print(f"🥳 Running Expense App")
     # Get input for expense
-    
+    get_expenses()
     # Write expense to file 
-    
+    write_expense_to_file()
     # Read file and summarize 
+    read_file_and_summarize()
+    
     pass
 
+
+
+def get_expenses():
+     print(f"🫡 Running get expense function")
+     pass
+ 
+def write_expense_to_file():
+    print(f" 😱 Running get write to file function")
+    
+    
+
+def read_file_and_summarize():
+    print(f" 🫥 Running get summarize function")
+    
 
 if __name__ == "__main__":
     main()
